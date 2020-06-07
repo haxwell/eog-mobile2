58d6aec8731456c5c85c0f6e8cf4ad8b6457f4a7
Johnathan James <haxwell@gmail.com>
2020-06-07 08:57:22 -0600

    TutorialBasicConceptsPage, corrected no provider error for HttpClient and ngModel

--- src/app/about-easyah/tutorials/tutorial-basic-concepts/tutorial-basic-concepts.page.spec.ts
+++ src/app/about-easyah/tutorials/tutorial-basic-concepts/tutorial-basic-concepts.page.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { TutorialBasicConceptsPage } from './tutorial-basic-concepts.page';
@@ -10,7 +12,7 @@ describe('TutorialBasicConceptsPage', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ TutorialBasicConceptsPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), FormsModule, HttpClientModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(TutorialBasicConceptsPage);
