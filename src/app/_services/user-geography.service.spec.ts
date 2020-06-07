435446b14f7c3493f986c7ba1db826e9371a60ab
Johnathan James <haxwell@gmail.com>
2020-06-06 23:21:30 -0600

    Added HttpClient provider for user-geography.service.spec

--- src/app/_services/user-geography.service.spec.ts
+++ src/app/_services/user-geography.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { UserGeographyService } from './user-geography.service';
 
 describe('UserGeographyService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+  	imports: [ HttpClientModule ]
+  }));
 
   it('should be created', () => {
     const service: UserGeographyService = TestBed.get(UserGeographyService);
