4840e027c80a6c228b7b4685a61efaa1ed22cd3e
Johnathan James <haxwell@gmail.com>
2020-06-07 09:09:09 -0600

    outgoingrequest page, got test goin.

--- src/app/about-easyah/tutorials/tutorial-outgoing-request-made/tutorial-outgoing-request-made.page.spec.ts
+++ src/app/about-easyah/tutorials/tutorial-outgoing-request-made/tutorial-outgoing-request-made.page.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { TutorialOutgoingRequestMadePage } from './tutorial-outgoing-request-made.page';
@@ -10,7 +12,7 @@ describe('TutorialOutgoingRequestMadePage', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ TutorialOutgoingRequestMadePage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), FormsModule, HttpClientModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(TutorialOutgoingRequestMadePage);
