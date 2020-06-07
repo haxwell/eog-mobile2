f04e8a9263a0d4e8ce84ca03fd5aa708d13e9980
Johnathan James <haxwell@gmail.com>
2020-06-06 23:43:37 -0600

    Error in privacy policy page, the Router did not have a provider. Imported RouterTestingModule

--- src/app/about-easyah/privacy-policy/privacy-policy.page.spec.ts
+++ src/app/about-easyah/privacy-policy/privacy-policy.page.spec.ts
@@ -1,7 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { PrivacyPolicyPage } from './privacy-policy.page';
+
+import { EasyahHeaderModule } from '../../easyah-header/easyah-header.module';
 
 describe('PrivacyPolicyPage', () => {
   let component: PrivacyPolicyPage;
@@ -10,7 +13,7 @@ describe('PrivacyPolicyPage', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ PrivacyPolicyPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), EasyahHeaderModule, RouterTestingModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(PrivacyPolicyPage);
