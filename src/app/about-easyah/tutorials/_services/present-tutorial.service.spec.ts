2b6049b71ee87ff70d01486982921ba49ae7f4a2
Johnathan James <haxwell@gmail.com>
2020-06-06 23:56:49 -0600

    Present Tutorial Service spec, added provider for ModalController. A mock ModalController with only the create() method

--- src/app/about-easyah/tutorials/_services/present-tutorial.service.spec.ts
+++ src/app/about-easyah/tutorials/_services/present-tutorial.service.spec.ts
@@ -2,8 +2,14 @@ import { TestBed } from '@angular/core/testing';
 
 import { PresentTutorialService } from './present-tutorial.service';
 
+import { ModalController } from '@ionic/angular'
+
 describe('PresentTutorialService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+  	providers: [
+  		{ provide: ModalController, useValue: { create: (options) => { }} }
+  	]
+  }));
 
   it('should be created', () => {
     const service: PresentTutorialService = TestBed.get(PresentTutorialService);
