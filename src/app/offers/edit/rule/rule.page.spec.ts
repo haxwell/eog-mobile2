7e4cac3ba5cf203c77e2e72c191cda6635b7037a
Johnathan James <haxwell@gmail.com>
2020-06-13 12:19:21 -0600

    Rule Page, getting specs goin

--- src/app/offers/edit/rule/rule.page.spec.ts
+++ src/app/offers/edit/rule/rule.page.spec.ts
@@ -1,16 +1,42 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Location } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { EasyahHeaderModule } from '../../../easyah-header/easyah-header.module';
+
+import { FileTransfer, FileTransferObject } from '@ionic-native/file-transfer/ngx';
+import { File } from '@ionic-native/file/ngx'
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Constants } from '../../../../_constants/constants';
+
+import { OfferModelService } from '../../../_services/offer-model.service'
+import { OfferModelServiceMock }  from '../../../../../test-config/mocks-easyah'
 
 import { RulePage } from './rule.page';
 
-describe('RulePage', () => {
+describe('Offers RulePage', () => {
   let component: RulePage;
   let fixture: ComponentFixture<RulePage>;
+
+  let offerModelServiceMock = new OfferModelServiceMock();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ RulePage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), ReactiveFormsModule, FormsModule, EasyahHeaderModule, HttpClientModule, RouterTestingModule]
+      ,providers: [
+        { provide: Location, useClass: Location }
+        ,{ provide: FileTransfer, useClass: FileTransfer }
+        ,{ provide: File, useClass: File }
+        ,{ provide: Constants, useClass: Constants }
+        ,{ provide: WebView, useValue: { convertFileSrc: (val) => { return val; }}}
+        ,{ provide: DomSanitizer, useValue: { sanitize: () => { }, bypassSecurityTrustResourceUrl: (val) => { return val; }}}
+        ,{ provide: OfferModelService, useValue: offerModelServiceMock }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(RulePage);
@@ -22,3 +48,4 @@ describe('RulePage', () => {
     expect(component).toBeTruthy();
   });
 });
+
