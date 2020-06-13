f177c299c2b4778b4c7f8d9ff57b145dc19ee144
Johnathan James <haxwell@gmail.com>
2020-06-13 11:28:17 -0600

    Fixed specs on decline.page

--- src/app/requests/incoming/decline/decline.page.spec.ts
+++ src/app/requests/incoming/decline/decline.page.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
 
 import { DeclinePage } from './decline.page';
 
@@ -10,7 +12,7 @@ describe('DeclinePage', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ DeclinePage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), FormsModule, RouterTestingModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(DeclinePage);
